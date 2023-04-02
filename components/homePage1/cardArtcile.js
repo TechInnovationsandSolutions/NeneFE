c6fd595ae1aad3fad5a8229c3e66436ff33a7b3c
Aksanti Bahiga tacite <71480535+marcellintacite@users.noreply.github.com>
2023-04-02 18:16:19 +0300

    Feat:adding the item component for the homepage

---
+++ components/homePage1/cardArtcile.js
@@ -0,0 +1,31 @@
+"use client";
+import Image from "next/image";
+import like from "../../public/assets/svgs/like.svg";
+import { useState } from "react";
+
+export default function CardArtcile({ item }) {
+  const [like, setLike] = useState(false);
+  const handleLike = () => {
+    setLike(!like);
+  };
+  return (
+    <div className="slide justify-center   relative  ml-4">
+      <div className="relative">
+        <Image src={item.img} alt="Slide" className="w-full" />
+        <div
+          onClick={handleLike}
+          className="absolute right-5 bottom-8 w-8 flex items-center justify-center bg-white h-8 rounded-full cursor-pointer"
+        >
+          <Image src={like} width={20} alt="like btn" height={18.48} />
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-between font-bold mt-3 text-lg">
+          <p className="text-md">{item.name}</p>
+          <p className="text-md">{item.price}</p>
+        </div>
+        <p className="text-gray-500">{item.model}</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Button from "@/components/button/button";
 import FirstSlider from "@/components/homePage1/FirstSlider";
-import SecondSlider from "@/components/homePage1/SecondSlide";
-import Shoes from "@/components/homePage1/Shoes";
-
 import Head from "next/head";
 import peso from "../components/homePage1/assets/persone.png";
 import Image from "next/image";
 import Brands from "@/components/homePage1/brand";
+import CategoryRender from "@/components/homePage1/categoryRender";
+import { shirt, shoes } from "@/components/homePage1/data";
 
 export default function Home() {
   return (
@@ -21,10 +20,10 @@ export default function Home() {
         <div className="w-full md:w-9/12 mx-auto mb-5">
           <FirstSlider />
         </div>
-        <section className="md:pt-20 pt-10">
+        <section className="md:pt-20 pt-10 mb-2">
           <h2 className="font-bold text-4xl text-center">New Now</h2>
-          <SecondSlider />
-          <Shoes />
+          <CategoryRender data={shirt} title="Shirt" />
+          <CategoryRender data={shoes} title="Shoes" />
         </section>
         <section
           className="bg-purle flex justify-center items-center p-2"
@@ -35,10 +34,10 @@ export default function Home() {
           <div className="md:mx-10 border-2 h-4/5 w-full border-white p-8 md:flex items-center">
             <div className="flex-1">
               <p className="text-2xl">
-                " I’m a product of in-depth discipline and growth. And after 30
+                I am a product of in-depth discipline and growth. And after 30
                 years of being in the entertainment industry, I can say that the
                 sense of fullfillment comes into play. But I still want to give
-                back to the society at large. "
+                back to the society at large
               </p>
               <div className="flex gap-4 items-center mt-5">
                 <div className="w-12 h-12 border-2 border-white rounded-full overflow-hidden md:hidden">
