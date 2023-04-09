186293cbcd569d6a0e4f506cf189912106aaca21
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2023-04-09 15:42:23 +0100

    pipeline fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#### Enter into NeneFE directory
 cd /home/ubuntu/NeneFE
+### Kill all running processes
 pm2 kill
+##remove previous .next file
 sudo rm -rf /home/ubuntu/NeneFE/.next
+##stop service
 sudo systemctl stop NeneFE.service
+##install and deploy new update
 npm install
 pm2 start "npm run  dev" --name "NeneFE"
