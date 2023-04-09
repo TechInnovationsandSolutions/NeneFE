0a26e149127a716b102a9d2f68f9bdfe19a85f94
Ola-Steeve <ekundayoolaoluwa69@gmail.com>
2023-04-09 17:28:30 +0100

    fix

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #### Enter into NeneFE directory
-cd /home/ubuntu/NeneFE
+
 ### Kill all running processes
 pm2 kill
 ##remove previous .next file
@@ -10,3 +10,20 @@ sudo systemctl stop NeneFE.service
 ##install and deploy new update
 npm install
 pm2 start "npm run  dev" --name "NeneFE"
+
+
+#!/bin/bash
+
+# Get the process ID of the Node.js server
+PID=$(ps -ef | grep node | grep -v grep | awk '{print $2}')
+
+# If the server is running, stop it
+if [ -n "$PID" ]; then
+  echo "Stopping Node.js server (PID $PID)..."
+  kill $PID
+  sleep 1
+fi
+
+# Start the server again
+echo "Starting Node.js server..."
+node /path/to/server.js
