dca3aacba3bea07a80deedbf43f71bb902ca331f
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2023-04-11 19:10:48 +0100

    Product page UI fourth commit

--- pages/product.js
+++ pages/product.js
@@ -12,32 +12,33 @@ function Product() {
   console.log(typeof img1);
   return (
     <article className='text-lg mx-[8%] lg:mx-24 font-sans font-[400]'>
-      <section>
-        <div>
-          <div>
-            <h3>Shopping Bag (1)</h3>
-            <Image src={close} />
+      <section className='fixed z-10 bg-black/[0.5] inset-0'>
+        <div className='z-20 w-[300px] sm:w-[474px] px-4 py-4 md:px-8 md:py-10 bg-white absolute'>
+          <div className='flex justify-between mb-10'>
+            <h3 className='font-bold text-2xl'>Shopping Bag (1)</h3>
+            <Image src={close} width={20} />
           </div>
-          <div>
-            <div className='max-w-[100px]'>
-                <Image src={dbress} width={100} height={100} />
+          <div className='flex gap-4 text-sm sm:text-base'>
+            <div className='w-[200px]'>
+                <Image src={dbress} />
             </div>
             <div>
-              <p>Zara Twist Front Textured Slinky Ruched Midi Dress </p>
-              <p>Color: Black</p>
-              <p>UK Size: 6</p>
-              <div>
-                <p>Quantity: 1</p>
-                <h5>N 3,000</h5>
+              <p className='text-[#3E3E3E] sm:text-lg'>Zara Twist Front Textured Slinky Ruched Midi Dress </p>
+              <p className='text-[#979797] mt-1 sm:mt-4 text-sm'>Color: Black</p>
+              <p className='text-[#979797] mt-1 sm:mt-4 text-sm'>UK Size: 6</p>
+              <div className='flex justify-between'>
+                <p className='text-[#979797] mt-1 sm:mt-4 text-sm'>Quantity: 1</p>
+                <h5 className='mt-1 sm:mt-4  font-bold'>N 3,000</h5>
               </div>
             </div>
-            <div></div>
-            <div>
-              <p>Total</p>
-              <h4>N 3,000</h4>
-            </div>
-            <Button type={'button'} customClassName={'text-xs'} variant={'black'} text='Checkout'/>
-            <Button type={'button'} customClassName={'text-xs'} variant={'white'} text='View Bag'/>
+          </div>
+          <div className='flex justify-between my-8 py-4 border-t border-[#979797]'>
+            <p className='text-[#3E3E3E] text-sm sm:text-base items-center align-middle'>Total</p>
+            <h4 className='text-lg font-bold sm:text-4xl'>N 3,000</h4>
+          </div>
+          <div className='flex flex-col gap-4'>
+            <Button type={'button'} customClassName={'w-full'} variant={'black'} text='Checkout'/>
+            <Button type={'button'} customClassName={'w-full border border-black'} variant={'white'} text='View Bag'/>
           </div>
         </div>
       </section>
