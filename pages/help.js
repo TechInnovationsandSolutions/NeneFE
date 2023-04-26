350c66dd0fa415c199268013fe54989ba3d3dc0c
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2023-04-26 14:45:30 +0100

    feat: second help page UI commit

--- pages/help.js
+++ pages/help.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Search from '@/section/pages/helpPage/search&help'
 import HelpInfo from '@/section/pages/helpPage/helpInfo'
+import HelpbottomSection from '@/section/pages/helpPage/helpbottomSection'
 
 const Help = () => {
   return (
+    <>
     <article className='text-sm mx-[30px] lg:mx-24 font-sans font-[400]'>
       <Search />
       <HelpInfo />
     </article>
+    <HelpbottomSection />
+    </>
   )
 }
 
