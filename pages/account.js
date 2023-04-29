cba667af8634de0d2bfd274ab493bbdee6c2a7ea
Prisca <priscaebube@gmail.com>
2023-04-29 22:22:36 +0100

    fix: Minor fix

--- pages/account.js
+++ pages/account.js
@@ -10,6 +10,7 @@ const Account = () => {
 
   return (
     <div className="w-full h-full flex flex-col bg-medGray px-5 py-5 lg:px-[100px] lg:py-[80px]">
+      <h1 className="text-[24px] font-bold text-center pb-16">MY ACCOUNT</h1>
       <div className=" w-full h-auto flex gap-[5%] bg-medGray ">
         <div className="w-[30%] h-auto">
           <SideBar />
