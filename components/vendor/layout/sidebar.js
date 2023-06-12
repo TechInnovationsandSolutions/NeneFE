484d0ec5828b1e137dda8b09edf47027731f44f1
TTUmejiaku <templeumejiaku@gmail.com>
2023-06-12 21:50:42 +0100

    feat: Completed the mobile view for the product table on vendor page

--- components/vendor/layout/sidebar.js
+++ components/vendor/layout/sidebar.js
@@ -1,61 +1,65 @@
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import dress2 from '../../../public/assets/images/dress2.webp'
-import bell from '../../../public/assets/svgs/bell.svg'
-import close from '../../../public/assets/svgs/close icon.svg'
-import { ShoppingCart } from '@/public/assets/svgs/js/ShoppingCart'
-import { CreditCard } from '@/public/assets/svgs/js/CreditCard'
-import { SignOut } from '@/public/assets/svgs/js/SignOut'
-import { Settings } from '@/public/assets/svgs/js/Settings'
-import { Slideshow } from '@/public/assets/svgs/js/Slideshow'
-import { QrCode } from '../../../public/assets/svgs/js/QrCode.js'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import dress2 from "../../../public/assets/images/dress2.webp";
+import bell from "../../../public/assets/svgs/bell.svg";
+import close from "../../../public/assets/svgs/close icon.svg";
+import { ShoppingCart } from "@/public/assets/svgs/js/ShoppingCart";
+import { CreditCard } from "@/public/assets/svgs/js/CreditCard";
+import { SignOut } from "@/public/assets/svgs/js/SignOut";
+import { Settings } from "@/public/assets/svgs/js/Settings";
+import { Slideshow } from "@/public/assets/svgs/js/Slideshow";
+import { QrCode } from "../../../public/assets/svgs/js/QrCode.js";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const List = [
-  {id:1, page:'Product', icon: QrCode, route:'product'},
-  {id:2, page:'Orders', icon: ShoppingCart, route:'orders'},
-  {id:3, page:'Payments', icon: CreditCard, route:'payments'},
-  {id:4, page:'Settings', icon: Settings, route:'settings'},
-  {id:5, page:'Enable Ads', icon: Slideshow, route:'ads'},
-  {id:6, page:'Log out', icon: SignOut, route:'products'},
-]
+  { id: 1, page: "Product", icon: QrCode, route: "product" },
+  { id: 2, page: "Orders", icon: ShoppingCart, route: "orders" },
+  { id: 3, page: "Payments", icon: CreditCard, route: "payments" },
+  { id: 4, page: "Settings", icon: Settings, route: "settings" },
+  { id: 5, page: "Enable Ads", icon: Slideshow, route: "ads" },
+  { id: 6, page: "Log out", icon: SignOut, route: "products" },
+];
 
-
-const Sidebar = ({openBar, setOpenBar}) => {
-
-  const {pathname} = useRouter()
-  let path = pathname.split('/')
+const Sidebar = ({ openBar, setOpenBar }) => {
+  const { pathname } = useRouter();
+  let path = pathname.split("/");
 
   useEffect(() => {
-    setCurrent(path[2])
-  }, [path])
+    setCurrent(path[2]);
+  }, [path]);
 
+  const [current, setCurrent] = useState(List[0].route);
 
-  const [current , setCurrent] = useState(List[0].route)
-
-  const onsetCurrent = (page) => setCurrent(page)
-  const closeBar = () => setOpenBar(false)
-
-
+  const onsetCurrent = (page) => setCurrent(page);
+  const closeBar = () => setOpenBar(false);
 
   return (
-    <div className={`h-screen overflow-y-auto scrollbar-hide lg:pt-[100px] min-w-[288px] fixed lg:border-r border-[#D7D7D7] ${openBar ? 'block' : 'hidden'} ${openBar ? 'z-30' : 'z-0'} lg:block bg-white`}>
-      <div className='p-6 flex justify-end pb-0 lg:hidden cursor-pointer' onClick={closeBar}>
-        <Image src={close} alt='close' />  
+    <div
+      className={`h-screen overflow-y-auto scrollbar-hide lg:pt-[100px] min-w-[288px] fixed lg:border-r border-[#D7D7D7] shadow-lg 2xl:shadow-none z-20 ${
+        openBar ? "block" : "hidden"
+      } ${openBar ? "z-30" : "z-0"} lg:block bg-white`}
+    >
+      <div
+        className='p-6 flex justify-end pb-0 lg:hidden cursor-pointer'
+        onClick={closeBar}
+      >
+        <Image src={close} alt='close' />
       </div>
-      <div className='py-6 flex flex-col gap-10'  >
+      <div className='py-6 flex flex-col gap-10'>
         {List.map((list, i) => (
-          <Link href={`/vendor/${list.route}`} onClick={closeBar} key={i} >
+          <Link href={`/vendor/${list.route}`} onClick={closeBar} key={i}>
             <div
-            className={` p-3 px-11  flex gap-8 items-center hover:bg-black/[0.05] ${current == list.route && 'lg:border-r border-black max-lg:border-l  text-black '} text-[#757575] cursor-pointer text-sm font-medium`} 
-            
-            onClick={e => onsetCurrent(list.route)}
+              className={` p-3 px-11  flex gap-8 items-center hover:bg-black/[0.05] ${
+                current == list.route &&
+                "lg:border-r border-black max-lg:border-l  text-black "
+              } text-[#757575] cursor-pointer text-sm font-medium`}
+              onClick={(e) => onsetCurrent(list.route)}
             >
-              <list.icon color={current == list.route ? '#000000' : '#cfcfcf'} />
+              <list.icon
+                color={current == list.route ? "#000000" : "#cfcfcf"}
+              />
               <p>{list.page}</p>
-
             </div>
           </Link>
         ))}
@@ -71,12 +75,14 @@ const Sidebar = ({openBar, setOpenBar}) => {
           </div>
           <div>
             <p className='font-medium text-base'>Namegoes here</p>
-            <p className='font-normal text-base text-[#757575]'>Name@emailadd.com</p>
+            <p className='font-normal text-base text-[#757575]'>
+              Name@emailadd.com
+            </p>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- sections/pages/vendor/product/ProductListing.js
+++ sections/pages/vendor/product/ProductListing.js
@@ -1,9 +1,16 @@
-import React, { useMemo, useState, useRef, useEffect } from "react";
+import React, { useMemo, useState } from "react";
+import {
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { plus } from "@/public/assets/svgs";
 import Image from "next/image";
 import ProductTable from "./ProductTable";
 import ProductCard from "./ProductCard";
 import axios from "axios";
+import productData from "@/mockData/productData.json";
+import { COLUMNS } from "./columns";
 
 const baseURL = "https://staging.nene.ng/api";
 const payload = "";
@@ -14,6 +21,11 @@ const headers = {
 };
 
 function ProductListing() {
+  const columns = useMemo(() => COLUMNS, []);
+  const [data, setData] = useState(() => [...productData].slice(0, 10));
+  const [sorting, setSorting] = useState();
+  const [isCellEditing, SetIsCellEditing] = useState(false);
+
   // try {
   //   const vendorProducts = axios(`${baseURL}/products/my-products`, payload, {
   //     headers,
@@ -23,21 +35,62 @@ function ProductListing() {
   //   console.log(error);
   // }
 
+  const table = useReactTable({
+    data,
+    columns,
+    columnResizeMode: "onChange",
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    meta: {
+      updateData: (rowIndex, columnId, value) => {
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex],
+                [columnId]: value,
+              };
+            }
+            return row;
+          })
+        );
+      },
+      isCellEditing,
+      SetIsCellEditing,
+      handleEditCellData: () => {
+        SetIsCellEditing(!isCellEditing);
+      },
+      deleteRow: (rowIndex) => {
+        setData(data.filter((product, i) => i !== rowIndex));
+      },
+    },
+  });
+
   return (
     <>
       <section className=''>
         <div className='w-11/12 mx-auto mt-[52px] lg:mt-9 mb-5'>
           <div className='md:flex items-center justify-between'>
             <p className='text-xl lg:text-2xl font-medium'>Product listing</p>
-            <div className='absolute bottom-[10%] right-[5%] md:relative md:bottom-0 md:right-0 md:w-[233px] flex items-center justify-evenly bg-black text-white w-[52px] h-[52px] rounded-full md:rounded-[4px] cursor-pointer'>
-              <Image src={plus} alt='' className='w-4 h-4' />
+            <div className='absolute bottom-[10%] right-[5%] flex items-center justify-evenly bg-black text-white w-[52px] h-[52px] rounded-full z-10 md:relative md:bottom-0 md:right-0 md:w-[233px] md:rounded-[4px] cursor-pointer'>
+              <Image src={plus} alt='plus icon' className='w-4 h-4' />
               <p className='hidden md:block '>Create New Product</p>
             </div>
           </div>
 
           {/* Table */}
-          <ProductTable tableClass='hidden lg:table w-full mt-10 ' />
-          <ProductCard productCardClass=' mt-10 space-y-5 lg:hidden' />
+          <ProductTable
+            tableClass='hidden lg:table w-[1000px] xl:w-full mt-10 overflow-x-auto'
+            table={table}
+          />
+          <ProductCard
+            productCardClass=' mt-10 space-y-5 lg:hidden'
+            table={table}
+          />
         </div>
       </section>
     </>
