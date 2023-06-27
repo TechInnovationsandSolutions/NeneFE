62fd224b33b1c68ae9dbdd1bd29e12f50de884f6
Fauzurraheem <odefunsofauzurraheem@gmail.com>
2023-06-27 06:11:22 +0100

    order page styles changed

--- pages/vendor/orders/index.js
+++ pages/vendor/orders/index.js
@@ -22,10 +22,10 @@ const Order = () => {
     <article>
       <div className='flex flex-col md:flex-row justify-between gap-3 md:items-center mt-8 mb-10 px-6'>
         <h2  className='text-2xl '>List of Orders <span className='text-[#757575]'>({list.length})</span></h2>
-        <div className='py-3 px-5 flex border justify-center border-[#AAAAAA] relative gap-3 cursor-pointer'  >
+        <div className='py-3 px-5 flex border justify-center lg:justify-between gap-3 lg:gap-0  border-[#AAAAAA] lg:w-[147px]  relative  cursor-pointer'  >
           <p>{period}</p>
           <Image src={chevronDown} onClick={showDecision}/>
-            <div className={`absolute -right-3 -bottom-36 ${show ? 'flex' : 'hidden'} flex-col shadow-md border text-sm bg-white w-44 text-center py-2`}>
+            <div className={`absolute right-0 -bottom-[132px] ${show ? 'flex' : 'hidden'} flex-col shadow-md border text-sm bg-white w-[147px] text-center py-2`}>
               <p className=' px-2 py-1 border-r text-[#757575]  hover:text-black cursor-pointer' onClick={e => pickPeriod('All Time')}>All Time</p>
               <p className=' px-2 py-1 border-r text-[#757575]  hover:text-black cursor-pointer' onClick={e => pickPeriod('This Year')}>This Year</p>
               <p className=' px-2 py-1 text-[#757575]  hover:text-black cursor-pointer' onClick={e => pickPeriod('This Month')}>This Month</p>
